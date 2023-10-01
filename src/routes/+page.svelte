0847f2391c62f7ef52148f4a59177bcf3595ddd8
Ankur Dutta <ankur.611@gmail.com>
2023-10-02 02:33:53 +0530

    Tailwind and DaisyUI added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
 Hello world!
 </h1>
 
+<button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+    Button
+</button>
+
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
