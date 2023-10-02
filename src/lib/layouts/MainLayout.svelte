b25d37ceb1b4eb9569b28ae5e6b534448fde4868
Ankur Dutta <ankur.611@gmail.com>
2023-10-02 17:40:06 +0530

    Component created for Layout

---
+++ src/lib/layouts/MainLayout.svelte
@@ -0,0 +1,24 @@
+<section>
+  <div class="header border">
+    <slot name="header" />
+  </div>
+
+  <div class="body overflow-y-auto border border-red-500">
+    <slot name="body" />
+  </div>
+
+
+  <div class="footer border border-blue-500">
+    <slot name="footer"/>
+  </div>
+</section>
+
+<style lang="postcss">
+  :global(html) {
+      background-color: theme(colors.gray.100);
+  }
+  .body {
+      height: 80vh;
+  }
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,20 @@
 <script>
     import Greet from "$lib/Greet.svelte";
+    import MainLayout from "$lib/layouts/MainLayout.svelte";
 </script>
 
-<section>
-  <div class="header border"> Screens </div>
+<MainLayout>
+  <h1 slot="header">Screens</h1>
 
-  <div class="body overflow-y-scroll border border-red-500">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aperiam commodi doloribus esse est id in, inventore itaque, quasi similique voluptatum? Distinctio ex mollitia nam quo recusandae totam voluptatibus.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, corporis cum dignissimos eos eum iste molestias mollitia nemo numquam quae quis quos? Assumenda natus nulla omnis, placeat porro totam!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate eos facilis in incidunt ipsa, nobis perspiciatis placeat quasi quibusdam quod, repudiandae soluta. Ad dignissimos, doloribus illo natus quaerat qui?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto iure non tenetur! Adipisci blanditiis cupiditate fugiat, illum inventore minima officia, perspiciatis provident qui sapiente sequi similique sint sit veniam voluptatibus?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consectetur culpa cupiditate debitis deleniti deserunt dolor eos ipsam molestias nam nemo obcaecati saepe sequi similique sunt tenetur velit voluptatibus voluptatum!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci at atque consectetur earum et expedita iste obcaecati officiis placeat, porro quaerat quidem repellat, reprehenderit sapiente tempore vitae, voluptatibus!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolorum eos explicabo magnam maiores possimus quaerat quas reiciendis totam voluptates. Deserunt neque omnis provident quibusdam quisquam? Necessitatibus nostrum quibusdam reiciendis!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab et in, molestiae odio reprehenderit veniam! Commodi ipsum perferendis possimus voluptatem. Modi obcaecati ut vel. Beatae facilis iure saepe voluptas voluptatum.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aperiam commodi doloribus esse est id in, inventore itaque, quasi similique voluptatum? Distinctio ex mollitia nam quo recusandae totam voluptatibus.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, corporis cum dignissimos eos eum iste molestias mollitia nemo numquam quae quis quos? Assumenda natus nulla omnis, placeat porro totam!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate eos facilis in incidunt ipsa, nobis perspiciatis placeat quasi quibusdam quod, repudiandae soluta. Ad dignissimos, doloribus illo natus quaerat qui?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto iure non tenetur! Adipisci blanditiis cupiditate fugiat, illum inventore minima officia, perspiciatis provident qui sapiente sequi similique sint sit veniam voluptatibus?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consectetur culpa cupiditate debitis deleniti deserunt dolor eos ipsam molestias nam nemo obcaecati saepe sequi similique sunt tenetur velit voluptatibus voluptatum!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci at atque consectetur earum et expedita iste obcaecati officiis placeat, porro quaerat quidem repellat, reprehenderit sapiente tempore vitae, voluptatibus!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolorum eos explicabo magnam maiores possimus quaerat quas reiciendis totam voluptates. Deserunt neque omnis provident quibusdam quisquam? Necessitatibus nostrum quibusdam reiciendis!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab et in, molestiae odio reprehenderit veniam! Commodi ipsum perferendis possimus voluptatem. Modi obcaecati ut vel. Beatae facilis iure saepe voluptas voluptatum.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aperiam commodi doloribus esse est id in, inventore itaque, quasi similique voluptatum? Distinctio ex mollitia nam quo recusandae totam voluptatibus.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, corporis cum dignissimos eos eum iste molestias mollitia nemo numquam quae quis quos? Assumenda natus nulla omnis, placeat porro totam!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate eos facilis in incidunt ipsa, nobis perspiciatis placeat quasi quibusdam quod, repudiandae soluta. Ad dignissimos, doloribus illo natus quaerat qui?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto iure non tenetur! Adipisci blanditiis cupiditate fugiat, illum inventore minima officia, perspiciatis provident qui sapiente sequi similique sint sit veniam voluptatibus?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consectetur culpa cupiditate debitis deleniti deserunt dolor eos ipsam molestias nam nemo obcaecati saepe sequi similique sunt tenetur velit voluptatibus voluptatum!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci at atque consectetur earum et expedita iste obcaecati officiis placeat, porro quaerat quidem repellat, reprehenderit sapiente tempore vitae, voluptatibus!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolorum eos explicabo magnam maiores possimus quaerat quas reiciendis totam voluptates. Deserunt neque omnis provident quibusdam quisquam? Necessitatibus nostrum quibusdam reiciendis!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab et in, molestiae odio reprehenderit veniam! Commodi ipsum perferendis possimus voluptatem. Modi obcaecati ut vel. Beatae facilis iure saepe voluptas voluptatum.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aperiam commodi doloribus esse est id in, inventore itaque, quasi similique voluptatum? Distinctio ex mollitia nam quo recusandae totam voluptatibus.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, corporis cum dignissimos eos eum iste molestias mollitia nemo numquam quae quis quos? Assumenda natus nulla omnis, placeat porro totam!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate eos facilis in incidunt ipsa, nobis perspiciatis placeat quasi quibusdam quod, repudiandae soluta. Ad dignissimos, doloribus illo natus quaerat qui?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto iure non tenetur! Adipisci blanditiis cupiditate fugiat, illum inventore minima officia, perspiciatis provident qui sapiente sequi similique sint sit veniam voluptatibus?
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consectetur culpa cupiditate debitis deleniti deserunt dolor eos ipsam molestias nam nemo obcaecati saepe sequi similique sunt tenetur velit voluptatibus voluptatum!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci at atque consectetur earum et expedita iste obcaecati officiis placeat, porro quaerat quidem repellat, reprehenderit sapiente tempore vitae, voluptatibus!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolorum eos explicabo magnam maiores possimus quaerat quas reiciendis totam voluptates. Deserunt neque omnis provident quibusdam quisquam? Necessitatibus nostrum quibusdam reiciendis!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab et in, molestiae odio reprehenderit veniam! Commodi ipsum perferendis possimus voluptatem. Modi obcaecati ut vel. Beatae facilis iure saepe voluptas voluptatum.
-
+  <div slot="body">
+    
   </div>
 
-
-  <div class="footer border border-blue-500">
-    <div class="join">
-      <button class="btn join-item">Button</button>
-      <button class="btn join-item">Button</button>
-      <button class="btn join-item">Button</button>
-    </div>
+  <div slot="footer" class="join">
+    <button class="btn join-item">Button</button>
+    <button class="btn join-item">Button</button>
+    <button class="btn join-item">Button</button>
   </div>
-</section>
-
+</MainLayout>
 
 <!--<Greet />-->
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-  .body {
-      height: 80vh;
-  }
-</style>
