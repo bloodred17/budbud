83f5807203c459937b0d21749b8fa0603b37141f
Ankur Dutta <ankur.611@gmail.com>
2023-10-02 17:02:10 +0530

    Transaction serialization working

--- src-tauri/src/core/transaction.rs
+++ src-tauri/src/core/transaction.rs
@@ -1,15 +1,32 @@
 use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Serialize, Deserialize, Clone)]
 pub enum TransactionType {
     Income,
     Expense,
 }
 
+#[derive(Serialize, Deserialize, Clone)]
 pub struct TransactionFor {
     name: String,
     transaction_type: TransactionType,
 }
 
+impl TransactionFor {
+    pub fn new(
+        name: &String,
+        transaction_type: TransactionType,
+    ) -> TransactionFor {
+        TransactionFor {
+            name: name.to_owned(),
+            transaction_type,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Transaction {
     transaction_for: TransactionFor,
     amount: f64,
@@ -19,4 +36,32 @@ pub struct Transaction {
     start_date: Option<DateTime<Utc>>,
     end_date: Option<DateTime<Utc>>,
     chron_expression: Option<String>,
+}
+
+impl Transaction {
+    pub fn new(
+        transaction_for: &TransactionFor,
+        amount: f64,
+    ) -> Transaction {
+        Transaction {
+            transaction_for: transaction_for.clone(),
+            amount,
+            created_on: Utc::now(),
+            is_recurring: false,
+            start_date: Some(Utc::now()),
+            end_date: None,
+            chron_expression: None
+        }
+    }
+
+    pub fn set_recurring_charge(mut self, chron_expression: String, end_date: Option<DateTime<Utc>>) -> Transaction {
+        self.is_recurring = true;
+        self.chron_expression = Some(chron_expression);
+        if end_date != None {
+            self.end_date = end_date;
+        }
+        self
+    }
+
+
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use crate::core::transaction::{TransactionFor, TransactionType};
+
 pub mod core;
 
 #[tauri::command]
@@ -8,9 +10,21 @@ fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[tauri::command]
+fn create_transaction() -> String {
+    let transaction_for = TransactionFor::new(
+        &String::from("Credit Card"),
+        TransactionType::Expense,
+    );
+    return match serde_json::to_string(&transaction_for) {
+        Ok(x) => x,
+        Err(e) => String::from("")
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![greet])
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+        .invoke_handler(tauri::generate_handler![greet, create_transaction])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
