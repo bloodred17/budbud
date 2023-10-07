396d3cf0b5f817e7b8d0882f6035007308afbd0b
Ankur Dutta <ankur.611@gmail.com>
2023-10-07 18:06:52 +0530

    UI changes and refactoring

--- src/lib/components/AcceptCancelButtons.svelte
+++ src/lib/components/AcceptCancelButtons.svelte
@@ -2,16 +2,19 @@
   import {createEventDispatcher} from "svelte";
 
   const dispatch = createEventDispatcher();
+
+  let disableCancel = false;
+  let disableAccept = false;
 </script>
 
 <div class="join">
-  <button class="btn btn-sm join-item" on:click|preventDefault|stopPropagation={() => dispatch('cancel', true)}>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+  <button disabled={disableCancel} class="btn btn-sm join-item" on:click|preventDefault|stopPropagation={() => dispatch('cancel', true)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
   </button>
-  <button class="btn btn-sm join-item" on:click|preventDefault|stopPropagation={() => dispatch('accept', true)}>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+  <button disabled={disableAccept} class="btn btn-sm join-item" on:click|preventDefault|stopPropagation={() => dispatch('accept', true)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
     </svg>
   </button>
